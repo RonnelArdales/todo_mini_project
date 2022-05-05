9d7546fa498d7b27e749edfc41c3ca5e0017e574
RonnelArdales <ronnelardales2192@gmail.com>
2022-05-05 19:05:52 +0800

    final

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from ast import arg
 from email.mime import image
 from email.policy import default
 from django.db import models
@@ -11,12 +12,3 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
